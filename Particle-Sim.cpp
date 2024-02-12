675efb22e558c0e2b525e440ce5c21f442e6e92c
Jacob Miguel Villa <jacob_villa@dlsu.edu.ph>
2024-02-12 20:16:55 +0800

    add: particle class definition

--- Particle-Sim.cpp
+++ Particle-Sim.cpp
@@ -1,12 +1,66 @@
+#define _USE_MATH_DEFINES
+
 #include <iostream>
+#include <stdlib.h>
+#include <thread>
+#include <vector>
+#include <chrono>
+#include <string>
+#include <ctime>
+#include <cmath>
 
 #include <imgui.h>
 #include <imgui_impl_opengl3.h>
 #include <GLFW/glfw3.h>
-
 #include <imgui_impl_glfw.h>
 
+using namespace std;
+
 static GLFWwindow* window = nullptr;
+class Particle {
+public:
+	int x, y;
+	double angle; // Angle in degrees
+	double velocity; // Velocity in pixels per second
+
+	Particle(int x, int y, double angle, double velocity)
+		: x(x), y(y), angle(angle), velocity(velocity) {}
+
+	void UpdatePosition(double deltaTime) {
+		// Convert angle to radians
+		double radians = -angle * M_PI / 180.0;
+
+		// Calculate the change in position
+		double dx = cos(radians) * velocity * deltaTime;
+		double dy = sin(radians) * velocity * deltaTime;
+
+		// Update position
+		x += static_cast<int>(dx);
+		y += static_cast<int>(dy);
+
+		y = 720 - y; // Invert y-axis
+
+		// Bounce off the walls (ADJUST HERE)
+		if (x < 0) {
+			x = 0;
+			angle = 180 - angle; // Reflect angle
+		}
+		else if (x > 1280) {
+			x = 1280;
+			angle = 180 - angle;
+		}
+
+		if (y < 0) {
+			y = 0;
+			angle = -angle;
+		}
+		else if (y > 720) {
+			y = 720;
+			angle = -angle;
+		}
+		y = 720 - y; // Invert y-axis
+	}
+};
 
 static void GLFWErrorCallback(int error, const char* description)
 {
